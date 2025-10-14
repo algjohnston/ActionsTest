148f10a3ab197dd6c1934916805647f71e85c91e
Alex Johnston <johnsa66@uwosh.edu>
2025-10-14 13:29:40 -0500

    Added tests

---
+++ App/Program.cs
@@ -0,0 +1,14 @@
+﻿public class App()
+{
+
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+
+}
---
+++ TestApp/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿namespace TestApp;
+
+public class UnitTest1
+{
+    [Fact]
+    public void PassingTest()
+    {
+        App app = new();
+        Assert.Equal(3, app.add(1, 2));
+    }
+
+    [Fact]
+    public void FailingTest()
+    {
+        App app = new();
+        Assert.Equal(4, app.add(1, 2));
+    }
+}
