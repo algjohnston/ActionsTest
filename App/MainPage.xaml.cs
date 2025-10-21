94472b155e57e9f05c7a01e4fc0f0586207f304d
Alex Johnston <johnsa66@uwosh.edu>
2025-10-21 14:54:12 -0500

    Removed tests and extracted the button counting logic to business logic

---
+++ App/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace App;
+
+using BusinessLogic;
+
+public partial class MainPage : ContentPage
+{
+
+	readonly CounterService counterService = new();
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object? sender, EventArgs e)
+	{
+		var (count, message) = counterService.Increment();
+		CounterBtn.Text = message;
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
