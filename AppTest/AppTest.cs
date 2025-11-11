ab9be3d2abb4cb9d0171cbb990a3d316db05c565
Alex Johnston <johnsa66@uwosh.edu>
2025-11-11 13:51:50 -0600

    Added a new passing test

--- AppTest/AppTest.cs
+++ AppTest/AppTest.cs
@@ -1,11 +1,15 @@
-﻿namespace AppTest;
+﻿using App.BusinessLogic;
+
+namespace AppTest;
 
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void IncrementAdds1()
     {
-
+        CounterService counterService = new();
+        var (count, _) = counterService.Increment();
+        Assert.Equal(1, count);
     }
 }
